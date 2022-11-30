2b1b9a837277bde3ca0a6d9a722d06b7f0e04b1a
Daniele Fiorot <danyfiorot@gmail.com>
2022-11-30 12:17:43 -0300

    fixing fields scripts: enabling office.js

--- src/fields-values/fields-values.js
+++ src/fields-values/fields-values.js
@@ -1,9 +1,9 @@
-// Office.onReady((info) => {
-//   if (info.host === Office.HostType.Excel) {
-//     document.getElementById("btn_load").onclick = load;
-//     document.getElementById("projects").onclick = selectField('projects');
-//   }
-// });
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Excel) {
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("btn_load").onclick = load;
+  }
+});
 
 
 function selectedField(field) {
@@ -26,24 +26,24 @@ function selectedField(field) {
   }
 }
 
-// export async function load() {
-//   try {
-//     await Excel.run(async (context) => {
-//       /**
-//        * Insert your Excel code here
-//        */
-//       const range = context.workbook.getSelectedRange();
+export async function load() {
+  try {
+    await Excel.run(async (context) => {
+      /**
+       * Insert your Excel code here
+       */
+      const range = context.workbook.getSelectedRange();
 
-//       // Read the range address
-//       range.load("address");
+      // Read the range address
+      range.load("address");
 
-//       // Update the fill color
-//       range.format.fill.color = "yellow";
+      // Update the fill color
+      range.format.fill.color = "yellow";
 
-//       await context.sync();
-//       console.log(`The range address was ${range.address}.`);
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+      await context.sync();
+      console.log(`The range address was ${range.address}.`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
