992b5b62f4736fae501c4c8603696afb97a356ed
Daniele Fiorot <danyfiorot@gmail.com>
2022-11-29 21:47:12 -0300

    credentials and fields panels

---
+++ src/credentials/credentials.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global console, document, Excel, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Excel) {
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("btn_save").onclick = save;
+  }
+});
+
+export async function save() {
+  try {
+    await Excel.run(async (context) => {
+      /**
+       * Insert your Excel code here
+       */
+      const range = context.workbook.getSelectedRange();
+
+      // Read the range address
+      range.load("address");
+
+      // Update the fill color
+      range.format.fill.color = "yellow";
+
+      await context.sync();
+      console.log(`The range address was ${range.address}.`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
